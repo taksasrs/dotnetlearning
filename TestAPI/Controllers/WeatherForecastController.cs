d668e1f10d373986c575e442763a2bed8ecf3add
“taksasrs” <taksa@sirisoft.co.th>
2024-07-09 10:18:13 +0700

    [fix] test commit

--- TestAPI/Controllers/WeatherForecastController.cs
+++ TestAPI/Controllers/WeatherForecastController.cs
@@ -20,6 +20,7 @@ public class WeatherForecastController : ControllerBase
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
+
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
