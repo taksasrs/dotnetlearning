2b52a0d6bb81bbc88217c97a334e0ff3421c5d4a
teeradetsrs <teeradet@sirisoft.co.th>
2024-08-15 14:29:17 +0700

    update: redis port

--- TestAPI/Program.cs
+++ TestAPI/Program.cs
@@ -78,7 +78,7 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IWebRepository, WebRepository>();
 builder.Services.AddScoped<ITokenRepository, TokenRepository>();
 
-var redisConnectionString = builder.Configuration.GetConnectionString("Redis") ?? "ecommerce.redis.cache.windows.net";
+var redisConnectionString = builder.Configuration.GetConnectionString("Redis") ?? "localhost:6379";
 builder.Services.AddSingleton(new RedisCacheService(redisConnectionString));
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<TokenService>();
